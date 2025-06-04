b931b6601bb44eaaf0d28aa7f216eb72d60322d4
ESHWAR LADE <eshwar341arjun@gmail.com>
2025-06-04 13:18:14 +0530

    frontend deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -2,7 +2,7 @@ const {default: axios} = require("axios");
 
 
 
-export const BASE_URL="http://localhost:9080"
+export const BASE_URL="https://proconnect-qcex.onrender.com"
 
 export const clientServer=axios.create({
     baseURL:BASE_URL,
